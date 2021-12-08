2995518d364f9b3e86aff3bd31925aecc18f1135
DADGBE <futanaizi@gmail.com>
2021-12-08 22:40:48 +0900

    add responsive navbar

--- src/components/nav-component.js
+++ src/components/nav-component.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from "react";
+import { Nav, Navbar } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import AuthService from "../services/auth.services";
 import logo from "./logo.svg";
 
-const NavComponent = (props) => {
+export default function NavbarComponent(props) {
   let { currentUser, setCurrentUser } = props;
   const navigate = useNavigate();
   const handleLogout = async () => {
@@ -12,74 +13,61 @@ const NavComponent = (props) => {
     setCurrentUser(null);
     navigate("/");
   };
-
   return (
-    <nav>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <div className="navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link active" to="/">
-                  &nbsp;
-                  <img src={logo} alt="logo" width="40px" height="40px" />
-                  &nbsp;
-                </Link>
-              </li>
-              {!currentUser && (
-                <Fragment>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/register">
-                      Register
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/login">
-                      Login
-                    </Link>
-                  </li>
-                </Fragment>
-              )}
-              {currentUser && currentUser.user.role === "instructor" && (
-                <li className="nav-item">
-                  <Link className="nav-link" to="/postcourse">
-                    Post Course
-                  </Link>
-                </li>
-              )}
-              {currentUser && currentUser.user.role === "student" && (
-                <li className="nav-item">
-                  <Link className="nav-link" to="/enroll">
-                    Enroll
-                  </Link>
-                </li>
-              )}
-              {currentUser && (
-                <Fragment>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/course">
-                      Course
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/profile">
-                      Profile
-                    </Link>
-                  </li>
-
-                  <li className="nav-item">
-                    <Link onClick={handleLogout} className="nav-link" to="#">
-                      Logout
-                    </Link>
-                  </li>
-                </Fragment>
-              )}
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </nav>
+    <div className="App">
+      <Navbar
+        bg="dark"
+        variant="dark"
+        fixed="top"
+        expand="sm"
+        collapseOnSelect
+        style={{ paddingLeft: "1rem" }}
+      >
+        <Navbar.Brand>
+          <img src={logo} alt="logo" width="40px" height="40px" />
+        </Navbar.Brand>
+        <Navbar.Toggle className="coloring" />
+        <Navbar.Collapse>
+          <Nav>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            {!currentUser && (
+              <Fragment>
+                <Nav.Link as={Link} to="/login">
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to="/register">
+                  Register
+                </Nav.Link>
+              </Fragment>
+            )}
+            {currentUser && currentUser.user.role === "instructor" && (
+              <Nav.Link as={Link} to="/postcourse">
+                Post Course
+              </Nav.Link>
+            )}
+            {currentUser && currentUser.user.role === "student" && (
+              <Nav.Link as={Link} to="/enroll">
+                Enroll
+              </Nav.Link>
+            )}
+            {currentUser && (
+              <Fragment>
+                <Nav.Link as={Link} to="/course">
+                  Course
+                </Nav.Link>
+                <Nav.Link as={Link} to="/profile">
+                  Profile
+                </Nav.Link>
+                <Nav.Link as={Link} onClick={handleLogout} to="#">
+                  Logout
+                </Nav.Link>
+              </Fragment>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   );
-};
-
-export default NavComponent;
+}
